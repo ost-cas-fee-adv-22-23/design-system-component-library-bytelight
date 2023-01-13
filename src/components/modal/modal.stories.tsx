a2c7b52699036f8712c0f0aade18f58f6ed9ca10
Marco Baumgartner <marco.baumgartner94@gmail.com>
2023-01-13 14:51:54 +0100

    add button to modal story to showcase the modal and add close function to modal story

--- src/components/modal/modal.stories.tsx
+++ src/components/modal/modal.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from '../index';
 import { Modal as MumbleModal } from './modal';
 
 export default {
@@ -14,15 +15,17 @@ export default {
 
 } as ComponentMeta<typeof MumbleModal>;
 
-const Template: ComponentStory<typeof MumbleModal> = ({ onClose, children, title }) => (
+const Template: ComponentStory<typeof MumbleModal> = ({ onClose, children, title}) => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
   <>  
-    <div className="fixed inset-0 bg-violet-400 bg-opacity-50 z-10"></div>
-    <div className='flex justify-center'>
-     <MumbleModal onClose={onClose} title={title}>{children}</MumbleModal>
-  </div>
+  {isOpen? <MumbleModal onClose={() => setIsOpen(!isOpen)} title={title}>{children}</MumbleModal> : <Button as='button' onClick={() => setIsOpen(!isOpen)}>Open Modal</Button> }
   </>
 
-);
+)
+};
 export const Modal = Template.bind({});
 
 Modal.args = {
--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -7,15 +7,16 @@ type ModalProps = {
   title:string
 };
 
-export const Modal: FC<ModalProps> = ({onClose, children, title }) => {
+export const Modal: FC<ModalProps> = ({onClose, children, title}) => {
   useEffect(() => {
     document.body.classList.add('overflow-hidden');
     return () => document.body.classList.remove('overflow-hidden');
   }, []);
 
   return (
-    <div>
-      <div className="relative inline-flex flex-col border-violet-600 bg-white rounded-xl overflow-scroll top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+    <>
+     <div className="fixed inset-0 bg-violet-400 bg-opacity-50 z-10"></div>
+      <div className="fixed inline-flex flex-col border-violet-600 bg-white rounded-xl overflow-scroll top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
         <div className="inline-flex items-center bg-violet-600 py-m px-l gap-x-[165px] mb-l rounded-t-xl">
           <span className="text-3xl text-white">{title}</span>
           <button className="text-white" onClick={onClose}>
@@ -23,7 +24,7 @@ export const Modal: FC<ModalProps> = ({onClose, children, title }) => {
           </button>
         </div>
      {children}
-      </div>
-    </div>
+      </div> 
+    </>
   );
 };
