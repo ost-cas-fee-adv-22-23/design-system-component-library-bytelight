e5c1c1467da087b3cec4fdf4b40bee313b81ab37
matthias <matthias@smartive.ch>
2023-01-16 15:58:03 +0100

    add more generic share button

--- src/components/share-button/share-button.stories.tsx
+++ src/components/share-button/share-button.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import React, { useEffect, useState } from 'react';
 import { useCopyToClipboard } from 'react-use';
+import { ShareIcon } from '../icons/share';
 import { ShareButton as ShareButtonComponent } from './share-button';
 
 export default {
@@ -8,23 +9,31 @@ export default {
   component: ShareButtonComponent,
 } as ComponentMeta<typeof ShareButtonComponent>;
 
-const Template: ComponentStory<typeof ShareButtonComponent> = ({ onClick, isActive }) => {
+const Template: ComponentStory<typeof ShareButtonComponent> = (args) => {
   const [isCopied, setIsCopied] = useState(false);
   const [, copyToClipboard] = useCopyToClipboard();
   const link = 'https://smartive.ch/';
+
   useEffect(() => {
-    const time = setTimeout(() => setIsCopied(false), 2000);
+    setTimeout(() => setIsCopied(false), 2000);
   }, [isCopied]);
 
   return (
     <ShareButtonComponent
+      {...args}
       onClick={() => {
-        setIsCopied(!isCopied);
+        setIsCopied(true);
         copyToClipboard(link);
       }}
       isActive={isCopied}
+      icon={<ShareIcon size="16px" />}
     />
   );
 };
 
 export const ShareButton = Template.bind({});
+
+ShareButton.args = {
+  label: 'Copy Me pls',
+  labelTransition: 'Thanks!',
+};
--- src/components/share-button/share-button.tsx
+++ src/components/share-button/share-button.tsx
@@ -1,32 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { ShareIcon } from '../icons/share';
 
 type ShareButtonProps = {
   onClick: () => void;
+  icon: ReactNode;
   isActive: boolean;
+  label: string;
+  labelTransition: string;
 };
 
-export const ShareButton: FC<ShareButtonProps> = ({ onClick, isActive = false }) => {
+export const ShareButton: FC<ShareButtonProps> = ({
+  onClick,
+  isActive = false,
+  label = 'Copy Link',
+  labelTransition = 'Link copied',
+  icon,
+}) => {
   return (
-    <div>
-      {!isActive ? (
-        <button onClick={onClick} className="flex items-center text-slate-600 hover:bg-slate-100 hover:rounded-2xl">
-          <div className="flex items-center gap-x-xs p-xs hover:text-slate-700 ">
-            <ShareIcon size="16" />
-            <span>Copy Link</span>
-          </div>
-        </button>
-      ) : (
-        <button
-          onClick={onClick}
-          className="flex items-center text-slate-600 hover:bg-slate-100  hover:rounded-2xl focus:rounded-2xl focus:bg-slate-100"
-        >
-          <div className="flex items-center gap-x-xs p-xs hover:text-slate-700 ">
-            <ShareIcon size="16" />
-            <span>Link copied</span>
-          </div>
-        </button>
-      )}
-    </div>
+    <button onClick={onClick} className="flex items-center text-slate-600 hover:bg-slate-100 hover:rounded-2xl">
+      <div className="flex items-center gap-x-xs p-xs hover:text-slate-700 focus:rounded-2xl focus:bg-slate-100">
+        {icon}
+        <span>{!isActive ? label : labelTransition}</span>
+      </div>
+    </button>
   );
 };
